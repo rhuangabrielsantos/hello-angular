95a10463ee48ea55c74abae491ea35877ef032c1
Rhuan Gabriel <rhuan.santos@db1.com.br>
2024-01-17 16:32:09 -0300

    finish project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { LoaderComponent } from './components/Loader/loader.component';
+
+type AdviceType = {
+  id: number;
+  advice: string;
+}
+
+type SlipAdviceType = {
+  slip: AdviceType;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, HttpClientModule, LoaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'hello-angular';
+  advice: AdviceType | null = null;
+  loading: boolean = false;
+
+  constructor(
+    private readonly http_: HttpClient,
+  ) {}
+
+  ngOnInit() {
+    this.getRandomAdvice();
+  }
+
+  getRandomAdvice() {
+    this.loading = true;
+    this.http_.get<SlipAdviceType>('https://api.adviceslip.com/advice').subscribe(
+      (response) => {
+        this.advice = response.slip
+        this.loading = false
+      },
+    );
+  }
 }
---
+++ src/app/components/Loader/loader.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'loader',
+  standalone: true,
+  template: `<img src="../../assets/loading.svg" alt="">`,
+  styles: `
+    @keyframes rotate{
+      to{ transform: rotate(360deg); }
+    }
+
+    img {
+      animation: rotate 1.5s linear infinite;
+      width: 50px;
+      heigth: 50px;
+    }
+  `
+})
+export class LoaderComponent {}
